41e58b4ebe4b7343c9483e4111952b7cf978251a
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-21 21:24:14 +0530

    basic shared layout setup of dashboard

---
+++ src/pages/dashboard/SharedLayout.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
+
+const SharedLayout = () => {
+  return (
+    <>
+      <h1>SharedLayout</h1>
+      <Outlet />
+    </>
+  );
+};
+
+export default SharedLayout;
