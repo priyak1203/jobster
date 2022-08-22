0d985cd1d99393b671c160b8738145f49d774c41
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-22 23:33:30 +0530

    navlinks in small sidebar

--- src/assets/wrappers/SmallSidebar.js
+++ src/assets/wrappers/SmallSidebar.js
@@ -45,6 +45,33 @@ const Wrapper = styled.aside`
     display: flex;
     flex-direction: column;
   }
+  .nav-link {
+    display: flex;
+    align-items: center;
+    color: var(--grey-500);
+    padding: 1rem 0;
+    text-transform: capitalize;
+    transition: var(--transition);
+  }
+  .nav-link:hover {
+    color: var(--grey-900);
+  }
+  .nav-link:hover .icon {
+    color: var(--primary-500);
+  }
+  .icon {
+    font-size: 1.5rem;
+    margin-right: 1rem;
+    display: grid;
+    place-items: center;
+    transition: var(--transition);
+  }
+  .active {
+    color: var(--grey-900);
+  }
+  .active .icon {
+    color: var(--primary-500);
+  }
 `;
 
 export default Wrapper;
--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -4,6 +4,8 @@ import { FaTimes } from 'react-icons/fa';
 import Wrapper from '../assets/wrappers/SmallSidebar';
 import Logo from './Logo';
 import { toggleSidebar } from '../features/user/userSlice';
+import links from '../utils/links';
+import { NavLink } from 'react-router-dom';
 
 const SmallSidebar = () => {
   const { isSidebarOpen } = useSelector((store) => store.user);
@@ -28,8 +30,22 @@ const SmallSidebar = () => {
             <Logo />
           </header>
           <div className="nav-links">
-            <p>link 1</p>
-            <p>link 2</p>
+            {links.map((link) => {
+              const { id, text, path, icon } = link;
+              return (
+                <NavLink
+                  to={path}
+                  key={id}
+                  onClick={toggle}
+                  className={({ isActive }) =>
+                    isActive ? 'nav-link active' : 'nav-link'
+                  }
+                >
+                  <span className="icon">{icon}</span>
+                  {text}
+                </NavLink>
+              );
+            })}
           </div>
         </div>
       </div>
