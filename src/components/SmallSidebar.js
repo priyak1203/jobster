ecbe128704b0c97ffbf8150e7e4aa02f2e32b542
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-22 21:39:03 +0530

    small sidebar structure

--- src/components/SmallSidebar.js
+++ src/components/SmallSidebar.js
@@ -1,10 +1,28 @@
 import React from 'react';
+import { FaTimes } from 'react-icons/fa';
 import Wrapper from '../assets/wrappers/SmallSidebar';
+import Logo from './Logo';
 
 const SmallSidebar = () => {
   return (
     <Wrapper>
-      <h1>SmallSidebar</h1>;
+      <div className="sidebar-container show-sidebar">
+        <div className="content">
+          <button
+            className="close-btn"
+            onClick={() => console.log('close sidebar')}
+          >
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">
+            <p>link 1</p>
+            <p>link 2</p>
+          </div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
