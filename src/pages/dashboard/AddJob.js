4bf722b81bda25fbe3ef2d66c97c6a56d81be124
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-24 14:54:02 +0530

    add job basic

--- src/pages/dashboard/AddJob.js
+++ src/pages/dashboard/AddJob.js
@@ -1,7 +1,33 @@
 import React from 'react';
+import Wrapper from '../../assets/wrappers/DashboardFormPage';
+import { FormRow } from '../../components';
 
 const AddJob = () => {
-  return <h1>AddJob</h1>;
+  return (
+    <Wrapper>
+      <form className="form">
+        <h3>add job</h3>
+        <div className="form-center">
+          {/* position */}
+          <FormRow type="text" name="position" />
+          {/* company */}
+          <FormRow type="text" name="company" />
+          {/* job location */}
+          <FormRow type="text" name="jobLocation" labelText="job location" />
+          <div className="btn-container">
+            {/* clear button  */}
+            <button type="button" className="btn btn-block clear-btn">
+              clear
+            </button>
+            {/* submit button */}
+            <button type="submit" className="btn btn-block">
+              submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default AddJob;
