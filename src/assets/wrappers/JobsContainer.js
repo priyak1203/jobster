6b66c1fe62d4555b1559c8f3e4775ccda3653459
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-26 15:13:42 +0530

    loading component

---
+++ src/assets/wrappers/JobsContainer.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.section`
+  background-color: peachpuff;
+`;
+
+export default Wrapper;
--- src/components/JobsContainer.js
+++ src/components/JobsContainer.js
@@ -1,7 +1,35 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Wrapper from '../assets/wrappers/JobsContainer';
+import Job from './Job';
+import Loading from './Loading';
 
 const JobsContainer = () => {
-  return <h2>Jobs Container</h2>;
+  const { isLoading, jobs } = useSelector((store) => store.allJobs);
+
+  if (isLoading) {
+    return <Loading center />;
+  }
+
+  if (jobs.length === 0) {
+    return (
+      <Wrapper>
+        <h2>No jobs to display</h2>
+      </Wrapper>
+    );
+  }
+
+  return (
+    <Wrapper>
+      <h5>jobs info</h5>
+      <div>
+        {jobs.map((job) => {
+          console.log(job);
+          return <Job />;
+        })}
+      </div>
+    </Wrapper>
+  );
 };
 
 export default JobsContainer;
