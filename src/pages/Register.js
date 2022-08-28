6fd1ce3ef88fc5da59905822279dbdf9de85835c
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-28 20:17:57 +0530

    demo app test user login

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -84,6 +84,17 @@ const Register = () => {
         <button type="submit" className="btn btn-block" disabled={isLoading}>
           {isLoading ? 'loading...' : 'submit'}
         </button>
+        <button
+          type="button"
+          className="btn btn-block btn-hipster"
+          onClick={() =>
+            dispatch(
+              loginUser({ email: 'testUser@test.com', password: 'secret' })
+            )
+          }
+        >
+          {isLoading ? 'loading...' : 'demo app'}
+        </button>
         <p>
           {values.isMember ? 'Not a member yet?' : 'Already a member?'}
           <button type="button" className="member-btn" onClick={toggleMember}>
