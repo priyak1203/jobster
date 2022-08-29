1164e3c107463f2d60fc391b0ae7a282a1b3e5d4
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-29 17:06:57 +0530

    Area Chart and Bar Chart complete

--- src/components/AreaChart.js
+++ src/components/AreaChart.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import {
+  ResponsiveContainer,
+  AreaChart,
+  Area,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from 'recharts';
 
 const AreaChartComponent = ({ data }) => {
-  return <h1>AreaChartComponent</h1>;
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <AreaChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Area type="monotone" dataKey="count" stroke="#1e3a8a" fill="#3b82f6" />
+      </AreaChart>
+    </ResponsiveContainer>
+  );
 };
 
 export default AreaChartComponent;
--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import {
+  ResponsiveContainer,
+  BarChart,
+  Bar,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip,
+} from 'recharts';
 
 const BarChartComponent = ({ data }) => {
-  return <h1>BarChartComponent</h1>;
+  return (
+    <ResponsiveContainer width="100%" height={300}>
+      <BarChart data={data} margin={{ top: 50 }}>
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="date" />
+        <YAxis allowDecimals={false} />
+        <Tooltip />
+        <Bar dataKey="count" fill="#3b82f6" barSize={75} />
+      </BarChart>
+    </ResponsiveContainer>
+  );
 };
 
 export default BarChartComponent;
