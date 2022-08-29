09f32ec6c5ab11cfe510c6688cea05decd5ab13e
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-29 15:15:31 +0530

    stats container and stats item complete

--- src/components/StatsContainer.js
+++ src/components/StatsContainer.js
@@ -1,7 +1,43 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { FaBug, FaCalendarCheck, FaSuitcaseRolling } from 'react-icons/fa';
+import Wrapper from '../assets/wrappers/StatsContainer';
+import StatItem from './StatItem';
 
 const StatsContainer = () => {
-  return <h1>StatsContainer</h1>;
+  const { stats } = useSelector((store) => store.allJobs);
+
+  const defaultStats = [
+    {
+      title: 'pending applications',
+      count: stats.pending || 0,
+      icon: <FaSuitcaseRolling />,
+      color: '#e9b949',
+      bcg: '#fcefc7',
+    },
+    {
+      title: 'Interviews Scheduled',
+      count: stats.interview || 0,
+      icon: <FaCalendarCheck />,
+      color: '#647acb',
+      bcg: '#e0e8f9',
+    },
+    {
+      title: 'jobs declined',
+      count: stats.declined || 0,
+      icon: <FaBug />,
+      color: '#d66a6a',
+      bcg: '#ffeeee',
+    },
+  ];
+
+  return (
+    <Wrapper>
+      {defaultStats.map((item, index) => {
+        return <StatItem key={index} {...item} />;
+      })}
+    </Wrapper>
+  );
 };
 
 export default StatsContainer;
