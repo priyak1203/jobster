8d0450647da654021decc2c10d89d6d14e8f2fad
Priydarshini Karanth <kpriya1203@gmail.com>
2022-08-30 15:35:57 +0530

    search filters setup

--- src/components/SearchContainer.js
+++ src/components/SearchContainer.js
@@ -1,7 +1,71 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import Wrapper from '../assets/wrappers/SearchContainer';
+import FormRow from './FormRow';
+import FormRowSelect from './FormRowSelect';
 
 const SearchContainer = () => {
-  return <h2>Search Container</h2>;
+  const { isLoading, search, searchStatus, searchType, sort, sortOptions } =
+    useSelector((store) => store.allJobs);
+  const { jobTypeOptions, statusOptions } = useSelector((store) => store.job);
+
+  const handleSearch = (e) => {
+    console.log(e.target);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Wrapper>
+      <form className="form">
+        <h4>search form</h4>
+        <div className="form-center">
+          {/* search position */}
+          <FormRow
+            type="text"
+            name="search"
+            value={search}
+            handleChange={handleSearch}
+          />
+
+          {/* search by status */}
+          <FormRowSelect
+            name="searchStatus"
+            value={searchStatus}
+            list={[searchStatus, ...statusOptions]}
+            handleChange={handleSearch}
+            labelText="status"
+          />
+
+          {/* search by type */}
+          <FormRowSelect
+            name="searchType"
+            value={searchType}
+            list={[searchType, ...jobTypeOptions]}
+            handleChange={handleSearch}
+            labelText="type"
+          />
+
+          {/* sort */}
+          <FormRowSelect
+            name="sort"
+            value={sort}
+            list={sortOptions}
+            handleChange={handleSearch}
+          />
+          <button
+            className="btn btn-block btn-danger"
+            disabled={isLoading}
+            onClick={handleSubmit}
+          >
+            clear filters
+          </button>
+        </div>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default SearchContainer;
--- src/features/allJobs/allJobsSlice.js
+++ src/features/allJobs/allJobsSlice.js
@@ -2,11 +2,20 @@ import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 import { toast } from 'react-toastify';
 import customFetch from '../../utils/axios';
 
+const initialFiltersState = {
+  search: '',
+  searchStatus: 'all',
+  searchType: 'all',
+  sort: 'latest',
+  sortOptions: ['latest', 'oldest', 'a-z', 'z-a'],
+};
+
 const initialState = {
   isLoading: false,
   jobs: [],
   stats: {},
   monthlyApplications: [],
+  ...initialFiltersState,
 };
 
 export const getAllJobs = createAsyncThunk(
